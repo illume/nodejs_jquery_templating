f62d538fabc331ebc53cf755a4ca1b8514c96c87
Rene Dudfield <renesd@gmail.com>
2010-07-26 13:52:29 +0100

    readFiles function to read multiple files at once.  Added many TODOs.

--- run.js
+++ run.js
@@ -1,4 +1,7 @@
 
+
+
+
 // requires jsdom and node-htmlparser
 //   git clone http://github.com/tautologistics/node-htmlparser.git
 //   git clone http://github.com/tmpvar/jsdom.git
@@ -28,52 +31,101 @@ var templateUrl = "index.html";
 var docType = "";
 
 
-fs.readFile(__dirname + "/jquery.js", function(err, data) {
-  fs.readFile(__dirname + "/" + serverUrl, function(err2, serverSideData) {
-    fs.readFile(__dirname + "/" + templateUrl, function(err3, htmlData) {
-      fs.readFile(__dirname + "/" + jsonUrl, function(err4, jsonData) {
+var readFiles = function (files, whenDone) {
+    // readFiles (file, whenDone(theErrs, theDatas)) reads many files at once, 
+    //     and runs the whenDone function when all reading is done.
+
+    // To read many files at once.
+    var numFilesRead = 0;
+    var fileData = [];
+    var fileErr = [];
 
 
-	    //window.location.href = templateUrl;
-	    var serverSideJson = JSON.parse(jsonData); 
+    for(var i=0;i < files.length; i += 1) {
+        var fileName = files[i];
+        var idx = i;
+        fileData[fileName] = null;
+        fileErr[fileName] = null;
+        (function () {
+		var theFileName = fileName;
+		fs.readFile(fileName, function (err, data) {
+					  fileData[theFileName] = data;
+					  fileErr[theFileName] = err;
+					  numFilesRead += 1;
+					  if(numFilesRead == files.length) {
+						// all finished.
+						whenDone(fileErr, fileData);
+					  }
+		});
+        })();
+    }
 
-	    // Load the html in.
-	    //window.document.innerHTML = "<!DOCTYPE html>\n<html> <body> hi there.  </body> </html>";
-	    window.document.innerHTML = htmlData;
+}
 
 
-	    var env = {	window: window, 
+
+// TODO: allow passing in a list of .js files to run server side.
+var theFileNames = [__dirname + "/jquery.js", 
+			__dirname + "/" + serverUrl, 
+			__dirname + "/" + templateUrl, 
+			__dirname + "/" + jsonUrl];
+
+
+// TODO LATER: cache files if we have already read them.  Not too slow for now.
+readFiles(theFileNames, function (theErrs, theFiles) {
+
+	var data = theFiles[theFileNames[0]];
+	var serverSideData = theFiles[theFileNames[1]];
+	var htmlData = theFiles[theFileNames[2]];
+	var jsonData = theFiles[theFileNames[3]];
+
+
+	//sys.puts(data);
+
+	//window.location.href = templateUrl;
+
+	//TODO: cache json if we have already parsed it.
+	var serverSideJson = JSON.parse(jsonData); 
+
+
+	//TODO: cache html parsing.
+	// Load the html in.
+	//window.document.innerHTML = "<!DOCTYPE html>\n<html> <body> hi there.  </body> </html>";
+	window.document.innerHTML = htmlData;
+
+
+	var env = {	window: window, 
 			location: window.location, 
 			navigator: window.navigator,
-			"$" : window.jQuery,
-			};
+			"$" : window.jQuery };
 
-	    try {
-	      Script.runInNewContext(data.toString() + serverSideData.toString(), env);
-	    } catch(e){
-	      sys.puts(sys.inspect(e));
-	    }
+        //TODO: cache Context based on which libraries are used.
+	// eg, if just json changes, we should be fairly quick to already have jquery, and other libs loaded.
+	try {
+		Script.runInNewContext(data.toString() + serverSideData.toString(), env);
+	} catch(e){
+		sys.puts(sys.inspect(e));
+	}
 
-            window._renderServerSide(serverSideJson);
 
-	
-	    // To tell the client side we have processed the script server side already.
-	    // TODO: this fails with an error...
-	    //window.jQuery('body').append("<script type='text/javascript'>window._processedServerSide = true;</script>");
+	//TODO: should this run the onload onready events instead?
+	window._renderServerSide(serverSideJson);
 
-            var outputHtml = window.document.outerHTML;
 
-            //HACK: since the doc type is not output, we add it in here.
-            outputHtml = docType + outputHtml;
-            //HACK: since the above .append does not work, we hack it in here.
-            outputHtml = outputHtml.replace('</body>', "<script type='text/javascript'>window._processedServerSide = true;</script>\n</body>");
-	    sys.puts(outputHtml);
+	// To tell the client side we have processed the script server side already.
+	// TODO: this fails with an error...
+	//window.jQuery('body').append("<script type='text/javascript'>window._processedServerSide = true;</script>");
 
-      });
-    });
-  });
+	var outputHtml = window.document.outerHTML;
+
+	//HACK: since the doc type is not output, we add it in here.
+	outputHtml = docType + outputHtml;
+	//HACK: since the above .append does not work, we hack it in here.
+	outputHtml = outputHtml.replace('</body>', "<script type='text/javascript'>window._processedServerSide = true;</script>\n</body>");
+	sys.puts(outputHtml);
+
+
 });
-
 
 
 
